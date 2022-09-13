8b5caf8f0fbe2857b0e18a2bcfcf82f4b5fd0460
Milfer Muñoz <milfer0516@gmail.com>
2022-09-12 19:40:42 -0500

    Inicio proyecto con initializr aregando las dependencias

---
+++ proyecto_bubble_dog/src/test/java/com/bubble_dog/proyecto_bubble_dog/ProyectoBubbleDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bubble_dog.proyecto_bubble_dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBubbleDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
