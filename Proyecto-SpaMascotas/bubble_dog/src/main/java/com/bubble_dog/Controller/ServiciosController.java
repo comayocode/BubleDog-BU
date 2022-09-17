f88c2464463c884bd0578bf679d4be43f1aa1a8d
Rg-bit <rjim3nez@gmail.com>
2022-09-17 18:12:41 -0500

    Se agrago la logica de negocio a servicios

--- Proyecto-SpaMascotas/bubble_dog/src/main/java/com/bubble_dog/Controller/ServiciosController.java
+++ Proyecto-SpaMascotas/bubble_dog/src/main/java/com/bubble_dog/Controller/ServiciosController.java
@@ -1,7 +1,6 @@
 package com.bubble_dog.Controller;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- Proyecto-SpaMascotas/bubble_dog/src/main/java/com/bubble_dog/Models/ServiciosModel.java
+++ Proyecto-SpaMascotas/bubble_dog/src/main/java/com/bubble_dog/Models/ServiciosModel.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+
 @Entity
 @Table(name="servicio")
 public class ServiciosModel {
